ee81c46c3b03bf926cfd05f8a9c8a48c73220807
itsvarsharma <101050108+itsvarsharma@users.noreply.github.com>
2023-06-21 10:14:32 +0530

    Implemented all node sync for new peer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser=require("body-parser");
 const express = require("express");
+const request = require("request");
 const Blockchain = require('./blockchain')
 const PubSub= require("./publishsubscribe");
 
@@ -7,6 +8,8 @@ const app=express()
 const blockchain=new Blockchain();
 const pubsub=new PubSub(({blockchain}))
 
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS =`http://localhost:${DEFAULT_PORT}`;
 setTimeout(()=> pubsub.broadcastChain(),1000);
 
 app.use(bodyParser.json())
@@ -21,12 +24,22 @@ app.post("/api/mine",(req,res)=>{
     res.redirect("/api/blocks");
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = ()=>{
+   request({url:`${ROOT_NODE_ADDRESS}/api/blocks`},(error,response,body)=>{
+    if(!error && response.statusCode===200){
+        const rootChain =JSON.parse(body);
+        console.log('Replace chain on sync with',rootChain)
+        blockchain.replaceChain(rootChain)
+    }
+   }) 
+}
 let PEER_PORT;
 if(process.env.GENERATE_PEER_PORT==='true'){
     PEER_PORT=DEFAULT_PORT+Math.ceil(Math.random()*1000);
 }
 const PORT=PEER_PORT || DEFAULT_PORT;
 app.listen(PORT,()=>{
+
    console.log( `listening to PORT: ${PORT}`);
+   syncChains();
 });
